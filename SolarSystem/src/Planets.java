a2d8fe90fbcbfe485c874f699c17c3090a325520
George Cook <thegekko07@gmail.com>
2018-12-09 15:51:11 +0000

    The one that works

--- SolarSystem/src/Planets.java
+++ SolarSystem/src/Planets.java
@@ -2,26 +2,26 @@ import java.text.DecimalFormat;
 
 public class Planets {
 
-    private String planetName;
-    private double planetMass;
-    private double planetDistance;
-    private double orbitPeriodDouble;
+    private final String PLANET_NAME;
+    private final double PLANET_MASS;
+    private final double PLANET_DISTANCE;
+    private final double ORBIT_PERIOD_DOUBLE;
     private String habitable;
 
-    DecimalFormat df = new DecimalFormat("#.###");
+    DecimalFormat df = new DecimalFormat("0.0##");//Setting up the format for the decimal numbers.
 
-    public Planets(String name, double mass, double distance){
-        this.planetName = name;
-        this.planetMass = mass;
-        this.planetDistance = distance;
-        orbitPeriodDouble = calculateOrbit(planetDistance);
+    public Planets(String name, double mass, double distance){//Planet constructor.
+        this.PLANET_NAME = name;
+        this.PLANET_MASS = mass;
+        this.PLANET_DISTANCE = distance;
+        ORBIT_PERIOD_DOUBLE = calculateOrbit(PLANET_DISTANCE);
     }
 
-    public double calculateOrbit(double planetDistance){
+    public double calculateOrbit(double planetDistance){//Calculating orbit period.
         return Math.sqrt(planetDistance*planetDistance*planetDistance);
     }
 
-    public void ishabitable(double planetMass, double planetDistance, double luminosity){
+    public void ishabitable(double planetMass, double planetDistance, double luminosity){//The is habitable method.
         double distanceCalculationMin;
         double distanceCalculationMax;
 
@@ -37,7 +37,7 @@ public class Planets {
     }
 
     public String toString() {//Converting the value from HEX to a readable String.
-        return "Planet " + planetName + " has a mass of " + planetMass + " Earths, is "+ planetDistance + "AU from its star, and orbits in " + df.format(orbitPeriodDouble) + " years: could be habitable? " + habitable + "\n";
+        return "Planet " + PLANET_NAME + " has a mass of " + PLANET_MASS + " Earths, is "+ PLANET_DISTANCE + "AU from its star, and orbits in " + df.format(ORBIT_PERIOD_DOUBLE) + " years: could be habitable? " + habitable;
     }
 
 }
--- SolarSystem/src/SolarSystem.java
+++ SolarSystem/src/SolarSystem.java
@@ -2,31 +2,33 @@ import java.util.ArrayList;
 
 public class SolarSystem {
 
-    private String systemName;
-    private double starBrightness;
-    ArrayList<String> planetList = new ArrayList<>();
+    private final String SYSTEM_NAME;
+    private final double STAR_BRIGHTNESS;
+    ArrayList<String> planetList = new ArrayList<>(); //Setting up an array list for storing planet objects.
 
-    public SolarSystem(String name, double luminosity){
-        this.systemName = name;
-        this.starBrightness = luminosity;
+    public SolarSystem(String name, double luminosity){//System constructor.
+        this.SYSTEM_NAME = name;
+        this.STAR_BRIGHTNESS = luminosity;
     }
 
-    public void addPlanet(String name, double mass, double distance){
+    public void addPlanet(String name, double mass, double distance){//Adding a planet object.
         //planets.add(new Planets(name, mass, distance));
         Planets planet = new Planets(name, mass, distance);
-        planet.ishabitable(mass, distance, starBrightness );
+        planet.ishabitable(mass, distance, STAR_BRIGHTNESS ); //Running a method to check if the planet is habitable.
         planetList.add(planet.toString());
     }
 
 
     public String toString() {//Converting the value from HEX to a readable String.
-        String temp;
+        String outputString;
 
-        temp = systemName + "\n";
-        for(int i = 0; i >= planetList.size(); i++) {
-            temp = temp + planetList.get(i) + "\n";
+        outputString = SYSTEM_NAME + "\n";
+
+        for(String i : planetList){//Going through each element of the array list.
+            outputString = outputString + i;//Concatenating the element to the String that is going to be outputted.
+            outputString = outputString + "\n";
         }
-            return temp;
+            return outputString;
 
     }
 }
